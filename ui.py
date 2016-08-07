5f4d34f21ad6317efa8b7958afa9d5ea1dd0736b
Zachary Robbins <xaqiri@outlook.com>
2016-08-07 18:08:52 -0500

    Added SCREEN_OFFSET, adjusted board drawing
    Can now draw the gameboard starting at points other than (0, 0) using
    the SCREEN_OFFSET variable
    Game board isn't drawn if it goes past the ui area

--- ui.py
+++ ui.py
@@ -5,7 +5,7 @@ p.init()
 # Display more information on the tile that the mouse is on, such as entity stats, class, equipment, 
 
 class UI(): 
-    def __init__(self, window_size, board_size, font_size, tile_size, cursor): 
+    def __init__(self, window_size, board_size, font_size, tile_size, SCREEN_OFFSET, cursor): 
         self.edge = (window_size[0] // 4*3, 0), (window_size[0] // 4*3, window_size[1]) 
         self.board_size = board_size[0]*tile_size, board_size[1]*tile_size 
         self.ui_pos = window_size[0] // 4*3 + 10 
@@ -13,13 +13,15 @@ class UI():
         self.font = p.font.SysFont('comicsans', font_size) 
         self.tile_size = tile_size 
         self.cursor = cursor 
+        self.SCREEN_OFFSET = SCREEN_OFFSET 
 
     def render(self, screen, color, mouse_pos, game_board, actor_board): 
-        mouse_index = mouse_pos[0]//self.tile_size, mouse_pos[1]//self.tile_size 
         p.draw.line(screen, color, self.edge[0], self.edge[1], 2) 
-        screen.blit(self.font.render(str(mouse_pos), 1, color), (self.ui_pos, 10)) 
+        screen.blit(self.font.render(str((mouse_pos[0]+self.SCREEN_OFFSET[0], mouse_pos[1]+self.SCREEN_OFFSET[1])), 1, color), (self.ui_pos, 10)) 
+        mouse_index = mouse_pos[0]//self.tile_size, mouse_pos[1]//self.tile_size 
+        mouse_pos = mouse_pos[0], mouse_pos[1] 
          
-        if mouse_pos[0] < self.board_size[0] and mouse_pos[1] < self.board_size[1]: 
+        if mouse_pos[0] > 0 and mouse_pos[0] < self.board_size[0] and mouse_pos[1] > 0 and mouse_pos[1] < self.board_size[1]: 
             if type(actor_board[mouse_index[0]][mouse_index[1]]) != int: 
                 screen.blit(self.font.render(str(game_board[mouse_index[0]][mouse_index[1]]), 1, color), (self.ui_pos, 40)) 
                 screen.blit(self.font.render(str(actor_board[mouse_index[0]][mouse_index[1]]), 1, color), (self.ui_pos, 80))  
